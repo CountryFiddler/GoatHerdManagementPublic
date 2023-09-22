e3d03245fb5c9f877e3f051d117a4593105991c5
Ethan Gordash <71423061+CountryFiddler@users.noreply.github.com>
2023-09-22 11:23:35 -0700

    AWS Test Methods to List and Create ToDo's Complete

---
+++ src/graphql/mutations.js
@@ -0,0 +1,93 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createTodo = /* GraphQL */ `
+  mutation CreateTodo(
+    $input: CreateTodoInput!
+    $condition: ModelTodoConditionInput
+  ) {
+    createTodo(input: $input, condition: $condition) {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const updateTodo = /* GraphQL */ `
+  mutation UpdateTodo(
+    $input: UpdateTodoInput!
+    $condition: ModelTodoConditionInput
+  ) {
+    updateTodo(input: $input, condition: $condition) {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const deleteTodo = /* GraphQL */ `
+  mutation DeleteTodo(
+    $input: DeleteTodoInput!
+    $condition: ModelTodoConditionInput
+  ) {
+    deleteTodo(input: $input, condition: $condition) {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const createDoes = /* GraphQL */ `
+  mutation CreateDoes(
+    $input: CreateDoesInput!
+    $condition: ModelDoesConditionInput
+  ) {
+    createDoes(input: $input, condition: $condition) {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const updateDoes = /* GraphQL */ `
+  mutation UpdateDoes(
+    $input: UpdateDoesInput!
+    $condition: ModelDoesConditionInput
+  ) {
+    updateDoes(input: $input, condition: $condition) {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const deleteDoes = /* GraphQL */ `
+  mutation DeleteDoes(
+    $input: DeleteDoesInput!
+    $condition: ModelDoesConditionInput
+  ) {
+    deleteDoes(input: $input, condition: $condition) {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,75 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTodo = /* GraphQL */ `
+  subscription OnCreateTodo($filter: ModelSubscriptionTodoFilterInput) {
+    onCreateTodo(filter: $filter) {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const onUpdateTodo = /* GraphQL */ `
+  subscription OnUpdateTodo($filter: ModelSubscriptionTodoFilterInput) {
+    onUpdateTodo(filter: $filter) {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const onDeleteTodo = /* GraphQL */ `
+  subscription OnDeleteTodo($filter: ModelSubscriptionTodoFilterInput) {
+    onDeleteTodo(filter: $filter) {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const onCreateDoes = /* GraphQL */ `
+  subscription OnCreateDoes($filter: ModelSubscriptionDoesFilterInput) {
+    onCreateDoes(filter: $filter) {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const onUpdateDoes = /* GraphQL */ `
+  subscription OnUpdateDoes($filter: ModelSubscriptionDoesFilterInput) {
+    onUpdateDoes(filter: $filter) {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const onDeleteDoes = /* GraphQL */ `
+  subscription OnDeleteDoes($filter: ModelSubscriptionDoesFilterInput) {
+    onDeleteDoes(filter: $filter) {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
